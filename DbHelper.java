8b527e53510ed476f86f9fad310b96ba50472476
NgoDucLam <130113922+NgoDucLam@users.noreply.github.com>
2024-08-01 19:59:49 +0700

    Add files via upload

---
+++ DbHelper.java
@@ -0,0 +1,122 @@
+package com.example.duan1_nhom5.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "ChickenCoffee.db";
+    private static final int DATABASE_VERSION = 3;
+    public DbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableTheLoai = "CREATE TABLE THELOAI(maLoai INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "tenLoai TEXT);";
+        db.execSQL(createTableTheLoai);
+        db.execSQL("INSERT INTO THELOAI VALUES(1, 'Coffee Milk'), (2, 'Coffee Moka'), (3, 'Coffee Latte'), (4, 'Coffee Mocha');");
+
+//Bảng sản phẩm
+        String createTableSanPham = ("CREATE TABLE SanPham(\n" +
+                "MaSanPham INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "image BLOG,\n" +
+                "TenSanPham TEXT,\n" +
+                "Price double,\n" +
+                "MaLoai INTEGER REFERENCES THELOAI(maLoai),\n" +
+                "MoTa TEXT\n" +
+                ");");
+        db.execSQL(createTableSanPham);
+        db.execSQL("INSERT INTO SanPham (TenSanPham, Price, MaLoai, MoTa) VALUES ('Cà Phê Sữa Nóng', 30000, 1, 'Một ly cà phê sữa nóng');");
+        db.execSQL("INSERT INTO SanPham (TenSanPham, Price, MaLoai, MoTa) VALUES ('Cà Phê Moka Đá', 35000, 2, 'Một ly cà phê Moka đá');");
+        db.execSQL("INSERT INTO SanPham (TenSanPham, Price, MaLoai, MoTa) VALUES ('Cà Phê Latte', 40000, 3, 'Một ly cà phê Latte');");
+
+
+
+// Bảng chức vụ
+        String createTableChucVu = "CREATE Table ChucVu(\n" +
+                "MaChucVu INTEGER PRIMARY KEY,\n" +
+                "TenChucVu TEXT\n" +
+                ");";
+        db.execSQL(createTableChucVu);
+
+        String insert_chucvu = "INSERT INTO ChucVu VALUES (1,'Quản Lý'),(2,'Nhân Viên')";
+        db.execSQL(insert_chucvu);
+
+// Bảng User
+        String tableUser = "CREATE Table User (\n" +
+                "MaUser INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "FullName TEXT,\n" +
+                "Username TEXT,\n" +
+                "ChucVu INTEGER REFERENCES ChucVu(machucvu),\n" +
+                "Password TEXT,\n" +
+                "SDT TEXT,\n" +
+                "NamSinh INTEGER\n" +
+                ");";
+        db.execSQL(tableUser);
+        String insert_user = "INSERT INTO User VALUES (1,'Chicken Coffee','admin',1,'admin', 0332147469, 2004)";
+        db.execSQL(insert_user);
+
+// Bảng hóa đơn
+        String tableHoaDon = "CREATE Table HoaDon (\n" +
+                "MaHoaDon INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "MaUser INTEGER REFERENCES User(MaUser),\n" +
+                "TenKhachHang TEXT,\n" +
+                "NgayLapHD TEXT,\n" +
+                "MaGioHang INTEGER,\n" +
+                "TrangThai TEXT\n," +
+                "DiaChi TEXT\n" +
+                ");";
+        db.execSQL(tableHoaDon);
+
+// Bảng giỏ hàng
+        String tableGioHang = "CREATE Table GioHang (\n" +
+                "MaGioHang INTEGER,\n" +
+                "MaSanPham INTEGER REFERENCES SanPham(MaSanPham),\n" +
+                "SoLuong INTEGER,\n" +
+                "Size TEXT,\n" +
+                "DonGia DOUBLE\n" +
+                ");";
+        db.execSQL(tableGioHang);
+
+// Bảng lưu hóa đơn
+        String tableLuuHoaDon = "CREATE Table LuuHoaDon (\n" +
+                "maLuu INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "maHoaDon INTEGER REFERENCES HoaDon(MaHoaDon),\n" +
+                "maUser INTEGER REFERENCES User(MaUser),\n" +
+                "trangThai TEXT REFERENCES HoaDon(TrangThai),\n" +
+                "tenUser TEXT,\n" +
+                "tenKhachHang TEXT,\n" +
+                "NgayLapHD TEXT,\n" +
+                "maSP INTEGER,\n" +
+                "tenSP TEXT,\n" +
+                "soLuong INTEGER,\n" +
+                "size TEXT,\n" +
+                "donGia DOUBLE\n," +
+                "thanhTien DOUBLE\n," +
+                "diaChi TEXT\n" +
+                ");";
+        db.execSQL(tableLuuHoaDon);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String dropLoaiSP = "drop table if exists THELOAI";
+        db.execSQL(dropLoaiSP);
+        String dropSanPham = "drop table if exists SanPham";
+        db.execSQL(dropSanPham);
+        String dropChucVu = "drop table if exists ChucVu";
+        db.execSQL(dropChucVu);
+        String dropUser = "drop table if exists User";
+        db.execSQL(dropUser);
+        String dropHoaDon = "drop table if exists HoaDon";
+        db.execSQL(dropHoaDon);
+        String dropLuuHoaDon = "drop table if exists LuuHoaDon";
+        db.execSQL(dropLuuHoaDon);
+        String dropGioHang = "drop table if exists GioHang";
+        db.execSQL(dropGioHang);
+    }
+}
---
+++ HoaDon_Adapter.java
@@ -0,0 +1,61 @@
+package com.example.duan1_nhom5.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.duan1_nhom5.Dao.GioHang_Dao;
+import com.example.duan1_nhom5.Model.GioHang;
+import com.example.duan1_nhom5.R;
+
+import java.util.ArrayList;
+
+public class HoaDon_Adapter extends RecyclerView.Adapter<HoaDon_Adapter.ViewHolder>{
+    private final Context context;
+    private final ArrayList<GioHang> list;
+    GioHang_Dao gioHangDAO;
+
+    public HoaDon_Adapter(Context context, ArrayList<GioHang> list) {
+        this.context = context;
+        this.list = list;
+    }
+    @NonNull
+    @Override
+    public HoaDon_Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view_hoadon, parent, false);
+        gioHangDAO = new GioHang_Dao(view.getContext());
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HoaDon_Adapter.ViewHolder holder, int position) {
+        GioHang gioHang = list.get(position);
+        holder.txtHDTenSP.setText(gioHang.getTenSP() + "(" + gioHang.getSize() + ")");
+        double donGia = gioHang.getDonGia();
+        String outDonGia = String.format("%,.0f", donGia);
+        holder.txtHDDonGia.setText(outDonGia);
+        holder.txtHDSL.setText(gioHang.getSoLuong() + "");
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txtHDTenSP, txtHDDonGia, txtHDSL;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtHDTenSP = itemView.findViewById(R.id.txtHDTenSP);
+            txtHDDonGia = itemView.findViewById(R.id.txtHDDonGia);
+            txtHDSL = itemView.findViewById(R.id.txtHDSL);
+        }
+    }
+}
